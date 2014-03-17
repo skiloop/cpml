eb049bf4976cd96d64909b25c4180fcb3d2da529
skiloop <skiloop@126.com>
2014-03-17 17:13:03 +0800

    modified matlab codes

--- cpmlPerformanceTest.m
+++ cpmlPerformanceTest.m
@@ -4,25 +4,20 @@
 clear;
 
 %% reference computing
-zZoneSize=10;
-tZoneSize=50;
+zZoneSize=60;
+tZoneSize=30;
 fdtd1dCPML;
 aEx=cEx;
-save refRes;
+save refRes aEx;
 
-% %% no reflect computing
-% zZoneSize=100;
-% tZoneSize=50;
-% fdtd1dCPML;
-% save noReflect;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% reflect computing
+zZoneSize=10;
+tZoneSize=30;
+fdtd1dCPML;
+
 %% performance analysis
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 t=(1:totalTimeStep)*dt;
-
-%%
 load refRes aEx;
-load noReflect cEx;
 relativeError=abs(cEx-aEx)/max(abs(aEx));
 dbError=20*log10(relativeError);
 % comparision of Ex in time
@@ -33,17 +28,17 @@ ylabel('Ez');
 grid on;
 title('time domain compare');
 legend('analysis','cpml');
-% print -depsc -tiff -r300 cpml1dTimeCompare_r
-% print -dtiff -r300 cpml1dTimeCompare_r
-%% Relative Error
+%print -depsc -tiff -r300 cpml1dTimeCompare_r
+%print -dtiff -r300 cpml1dTimeCompare_r
+% Relative Error
 figure('NumberTitle','OFF','Name','Relative Error');
 semilogy(t/1e-9,relativeError,'LineWidth',2);
 xlabel('time (ns)');
 ylabel('Relative Error');
 grid on;
 title('Relative Error');
-% print -depsc -tiff -r300 cpml1dRelativeError_r
-% print -dtiff -r300 cpml1dRelativeError_r
+%print -depsc -tiff -r300 cpml1dRelativeError_r
+%print -dtiff -r300 cpml1dRelativeError_r
 % Error in DB
 figure('NumberTitle','OFF','Name','DB Error');
 plot(t/1e-9,dbError,'LineWidth',2);
@@ -51,8 +46,5 @@ xlabel('time (ns)');
 ylabel('Error (DB)');
 grid on;
 title('DB Error');
-% print -depsc -tiff -r300 cpml1dDBError_r
-% print -dtiff -r300 cpml1dDBError_r
-% show max error
-maxDBError=max(dbError)
-maxRelError=max(relativeError)
+%print -depsc -tiff -r300 cpml1dDBError_r
+%print -dtiff -r300 cpml1dDBError_r
--- fdtd1dCPML.m
+++ fdtd1dCPML.m
@@ -5,20 +5,20 @@
 % EMAIL:skiloop@126.com
 %
 
-% close all;
+clc
+close all;
 
 %% some constants
 mu_0 = 1.2566370614359173e-06;   %
 eps_0= 8.8541878176203892e-12;   %
 C=299792458.0;      % speed of light
-Z0=1/C/eps_0;
 
 %% wave definition
 amptidute=100;
 frequency=110E9;
 T=1/frequency;
 lambda=C*T;
-t0=4.0*T;
+t0=4*T;
 omega=2*pi*frequency;
 
 %% FDTD variables
@@ -29,23 +29,23 @@ else if zZoneSize<0
     end
 end
 if exist('tZoneSize','var')==0
-    tZoneSize=50;
-else if tZoneSize<50
-    tZoneSize=50;
+    tZoneSize=4;
+else if zZoneSize<0
+    tZoneSize=4;
     end
 end
 domainLength=zZoneSize*lambda;
 totalTime=tZoneSize*T;
-numberCellsPerWavelength=20;
+numberCellsPerWavelength=200;
 
 %% CPML parameters
-pmlWidth=10;
-pmlOrder=3; % m
+pmlWidth=40;
+pmlOrder=4;
 epsR=1;
-sigmaMaxDivSigmaOpt=0.55;
-kappaMax=0.1;
-alphaMax=0.1;
-alphaOrder=1; % ma
+sigmaMax=1;
+kappaMax=15;
+alphaMax=0.24;
+alphaOrder=1;
 
 %% domain definition
 dz=lambda/numberCellsPerWavelength;
@@ -54,7 +54,7 @@ totalTimeStep=floor(totalTime/dt);
 nz=floor(domainLength/dz+2*pmlWidth);
 nzp1=nz+1;
 nzm1=nz-1;
-ksource = floor(nzp1/2);
+ksource = nz-pmlWidth-35;
 %% FDTD EM field arrarys
 Ex=zeros(1,nzp1);
 Hy=zeros(1,nz);
@@ -72,18 +72,16 @@ Psi_Hzx_zn=zeros(1,pmlWidth);
 Psi_Hzx_zp=zeros(1,pmlWidth);
 
 %% initial PML update coeficients
-sigmaOpt=0.8*(pmlOrder+1)/(sqrt(epsR)*dz*Z0);
-sigmaMax=sigmaOpt*sigmaMaxDivSigmaOpt;
-%sigmaOpt=sigmaMax*(pmlOrder+1)/(sqrt(epsR)*150*pi*dz);
+sigmaOpt=sigmaMax*(pmlOrder+1)/(sqrt(epsR)*150*pi*dz);
 % zn side
 rho_e=((pmlWidth:-1:1)-0.75)/pmlWidth;
 rho_m=((pmlWidth:-1:1)-0.25)/pmlWidth;
-sigma_e=sigmaMax*abs(rho_e).^pmlOrder;
-sigma_m=sigmaMax*abs(rho_m).^pmlOrder;
+sigma_e=sigmaOpt*abs(rho_e).^pmlOrder;
+sigma_m=sigmaOpt*abs(rho_m).^pmlOrder;
 kappa_e=1+(kappaMax-1)*abs(rho_e).^pmlOrder;
 kappa_m=1+(kappaMax-1)*abs(rho_m).^pmlOrder;
-alpha_e=alphaMax*abs(1-rho_e).^alphaOrder;
-alpha_m=alphaMax*abs(1-rho_m).^alphaOrder;
+alpha_e=alphaMax*abs(rho_e).^pmlOrder;
+alpha_m=alphaMax*abs(rho_m).^pmlOrder;
 cpml_b_e_n=exp((-dt/eps_0)...
     *((sigma_e./kappa_e)+alpha_e));
 cpml_a_e_n=1/dz*(cpml_b_e_n-1).*sigma_e ...
@@ -92,8 +90,6 @@ cpml_b_m_n=exp((-dt/eps_0)...
     *((sigma_m./kappa_m)+alpha_m));
 cpml_a_m_n=1/dz*(cpml_b_m_n-1).*sigma_m ...
     ./(kappa_m.*(sigma_m+kappa_m.*alpha_m));
-
-
 b=0;%sigma_e*dt/2/eps_0;
 Cexe_zn=(1-b)./(1+b);
 CPsi_Ezy_zn=-dt./(1+b)/eps_0;
@@ -105,12 +101,12 @@ Chyex_zn=CPsi_Hzx_zn./kappa_m/dz;
 % zp side
 rho_e=((1:1:pmlWidth)-0.75)/pmlWidth;
 rho_m=((1:1:pmlWidth)-0.25)/pmlWidth;
-sigma_e=sigmaMax*abs(rho_e).^pmlOrder;
-sigma_m=sigmaMax*abs(rho_m).^pmlOrder;
+sigma_e=sigmaOpt*abs(rho_e).^pmlOrder;
+sigma_m=sigmaOpt*abs(rho_m).^pmlOrder;
 kappa_e=1+(kappaMax-1)*abs(rho_e).^pmlOrder;
 kappa_m=1+(kappaMax-1)*abs(rho_m).^pmlOrder;
-alpha_e=alphaMax*abs(1-rho_e).^alphaOrder;
-alpha_m=alphaMax*abs(1-rho_m).^alphaOrder;
+alpha_e=alphaMax*abs(rho_e).^pmlOrder;
+alpha_m=alphaMax*abs(rho_m).^pmlOrder;
 cpml_b_e_p=exp((-dt/eps_0)...
     *((sigma_e./kappa_e)+alpha_e));
 cpml_a_e_p=1/dz*(cpml_b_e_p-1).*sigma_e ...
@@ -141,13 +137,12 @@ muSource=dtDivEps0DivDz*amptidute * -2.0 /T/T;
 % initial plot
 figure;
 h=plot(Ex);
-%set(gca,'ylim',[-2 2.5]*1e15);
+set(gca,'ylim',[-2 2.5]*1e15);
 set(gca,'xlim',[pmlWidth (nz-pmlWidth)]);
 grid on;
 
 % point to test performace of CPML
 ic=ksource+30;
-%ic=ksource+numberCellsPerWavelength;
 cEx=zeros(1,totalTimeStep);
 
 % fdtd loop
@@ -157,11 +152,17 @@ for n=1:totalTimeStep
     %============================
     Psi_Hzx_zn=cpml_b_m_n.*Psi_Hzx_zn+cpml_a_m_n.*(Ex(2:pmlWidth+1)-Ex(1:pmlWidth));
     Psi_Hzx_zp=cpml_b_m_p.*Psi_Hzx_zp+cpml_a_m_p.*(Ex(nzp1-pmlWidth+1:nzp1)-Ex(nzp1-pmlWidth:nz));
-    
+    if sum(isnan(Psi_Hzx_zp))>0
+        display('nan found');
+    end
     Hy(1:pmlWidth)=Chyh_zn.*Hy(1:pmlWidth)+Chyex_zn.*(Ex(2:pmlWidth+1)-Ex(1:pmlWidth))+CPsi_Hzx_zn.*Psi_Hzx_zn;
-    
+    if sum(isnan(Hy))>0
+        display('nan found');
+    end
     Hy(nz-pmlWidth+1:nz)=Chyh_zp.*Hy(nz-pmlWidth+1:nz)+Chyex_zp.*(Ex(nzp1-pmlWidth+1:nzp1)-Ex(nzp1-pmlWidth:nz))+CPsi_Hzx_zp.*Psi_Hzx_zp;
-    
+    if sum(isnan(Hy))>0
+        display('nan found');
+    end
     % non pml region
     Hy(pmlWidth+1:nz-pmlWidth)=Chyh* Hy(pmlWidth+1:nz-pmlWidth)+Chyex*( Ex(pmlWidth+2:nzp1-pmlWidth)-Ex(pmlWidth+1:nz-pmlWidth)); 
     
@@ -170,11 +171,18 @@ for n=1:totalTimeStep
     %===========================
     Psi_Ezy_zn=cpml_b_e_n.*Psi_Ezy_zn+cpml_a_e_n.*(Hy(2:pmlWidth+1)-Hy(1:pmlWidth));
     Psi_Ezy_zp=cpml_b_e_p.*Psi_Ezy_zp+cpml_a_e_p.*(Hy(nzp1-pmlWidth:nz)-Hy(nz-pmlWidth:nzm1));
-    
+    if sum(isnan(Psi_Ezy_zp))>0
+        display('nan found');
+    end
     Ex(2:pmlWidth+1)=Cexe_zn.*Ex(2:pmlWidth+1)+Cexhy_zn.*(Hy(2:pmlWidth+1)-Hy(1:pmlWidth))+CPsi_Ezy_zn.*Psi_Ezy_zn;
-        
-    Ex(nzp1-pmlWidth:nz)=Cexe_zp.*Ex(nzp1-pmlWidth:nz)+Cexhy_zp.*(Hy(nzp1-pmlWidth:nz)-Hy(nz-pmlWidth:nzm1))+CPsi_Ezy_zp.*Psi_Ezy_zp;
     
+    if sum(isnan(Ex))>0
+        display('nan found');
+    end
+    Ex(nzp1-pmlWidth:nz)=Cexe_zp.*Ex(nzp1-pmlWidth:nz)+Cexhy_zp.*(Hy(nzp1-pmlWidth:nz)-Hy(nz-pmlWidth:nzm1))+CPsi_Ezy_zp.*Psi_Ezy_zp;
+    if sum(isnan(Ex))>0
+        display('nan found');
+    end
     % non pml region
     Ex(pmlWidth+2:nz-pmlWidth)=Cexe* Ex(pmlWidth+2:nz-pmlWidth)+Cexhy*( Hy(pmlWidth+2:nz-pmlWidth)-Hy(pmlWidth+1:nzm1-pmlWidth)); 
     
